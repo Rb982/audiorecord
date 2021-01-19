85d6e4a89a3ee88a1f32c65cf7a1d394d787ff8a
Robert Bacon <rb40345@gmail.com>
2021-01-19 21:02:32 +0000

    fixed some compile time errors, can print output, have unclear panic

--- audiorecord/src/main.rs
+++ audiorecord/src/main.rs
@@ -3,17 +3,19 @@ use alsa::{Direction, ValueOr};
 use alsa::pcm::{PCM, HwParams, Format, Access, State};
 
 fn main() {
-    let args = env::args();
+    let mut args = env::args();
     let _ = args.next(); //Throw away the filename
     let dev_name=args.next().expect("Insufficient arguments.  Must provide device name.");
     //Threshold will need to become an i16 eventually, but not necessary yet.
     let out_dev_name = args.next().expect("Insufficient arguments.  Must provide output device");
     //Bool is for the nonblock property*/
-    let dev_name="";
-    let out_dev_name="";
-   let pcm = PCM::new(&dev_name, Direction::Capture, false);
+ //   let dev_name="";
+   // let out_dev_name="";
+   let pcm = PCM::new(&dev_name, Direction::Capture, false).unwrap();
    let hwp = HwParams::any(&pcm).unwrap();
-    hwp.set_channels(1).unwrap();
+println!("Min channels: {}", hwp.get_channels_min().unwrap());
+println!("Max channels: {}", hwp.get_channels_max().unwrap());
+    hwp.set_channels(2).unwrap();
     hwp.set_rate(44100, ValueOr::Nearest).unwrap();
     hwp.set_format(Format::s16()).unwrap();
     hwp.set_access(Access::RWInterleaved).unwrap();
@@ -22,12 +24,12 @@ fn main() {
 
 
     let mut buf = [0i16; 1024];
-    let reads=io.readi(&buf).unwrap();
+    let reads=io.readi(&mut buf).unwrap();
     println!("Read {} frames", reads);
     println!("Received following data: {:#?}", buf);
     thread::sleep(time::Duration::from_millis(5000));
 
-    let writePCM=PCM::new(&out_dev_name, Direction::Playback, false);
+    let writePCM=PCM::new(&out_dev_name, Direction::Playback, false).unwrap();
 
     let ohwp = HwParams::any(&writePCM).unwrap();
     ohwp.set_channels(1).unwrap();
